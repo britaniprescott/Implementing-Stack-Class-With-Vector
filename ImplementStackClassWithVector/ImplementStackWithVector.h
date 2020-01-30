06aafc5fda8695c52968070de2352b0f03ad4de1
Britani Prescott <britanierin@me.com>
2020-01-30 12:07:36 -0600

    added private variables and public functions to header file

--- ImplementStackClassWithVector/ImplementStackWithVector.h
+++ ImplementStackClassWithVector/ImplementStackWithVector.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <vector>
+
+template <class T>
+class Stack {
+	private:
+		int position;
+
+	public:
+		bool isEmpty();
+		void push(T new_value);
+		void pop();
+		int top();
+		ostream& operator<<(ostream& os, const Stack& stack);
+};
